c5c08cecbfdc7e51defc0c27783af273de6e202a
christianutn <christian.javier.bergero@gmail.com>
2025-07-18 19:38:56 -0300

    Se agrega dockerfile a backend

--- backend/src/config/base_datos.ts
+++ backend/src/config/base_datos.ts
@@ -6,17 +6,17 @@ const {
   DB_NAME,
   DB_USER,
   DB_PASSWORD,
-  DB_HOST,
+  DB_CONNECTION_NAME,
   DB_PORT
 } = process.env;
 
 // Verifica que las variables de entorno estén definidas
-if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST || !DB_PORT) {
+if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_CONNECTION_NAME || !DB_PORT) {
   throw new AppError("Faltan variables de entorno para la conexión a la base de datos", 500);
 }
 
 const sequelize  = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
+  host: DB_CONNECTION_NAME,
   dialect: 'mysql',
   port: parseInt(DB_PORT, 10), // Asegúrate de que el puerto sea un número
   dialectOptions: {
